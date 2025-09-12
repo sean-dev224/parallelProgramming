b4fe433a7cb3a00ba40c8cd470fa9cc4602538df
Sean Devlin <sean.dev224@gmail.com>
2025-09-12 13:29:51 -0400

    seq-nbody ready to run on centaurus

---
+++ seq-nbody/batch_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --job-name=seq-mergesort
+#SBATCH --partition=Centaurus
+#SBATCH --time=00:40:00
+#SBATCH --mem=10G
+$HOME/parallelProgramming/seq-nbody/nbody.out $1 $2 $3 $4 $5
--- seq-nbody/nbody.cpp
+++ seq-nbody/nbody.cpp
@@ -1,43 +1,57 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <cmath>
 #include <random>
 #include <stdexcept>
+#include <chrono>
 
 const double G = 6.674e-11;
 const double SOFTENING_FACTOR = 0.0000001;
 
 //math helper functions
 std::vector<double> scalar_multiplication(const std::vector<double> &vector, double scalar) {
-    std::vector<double> ret;
+    std::vector<double> result;
     for(const double& v: vector) {
-        ret.push_back(v*scalar);
+        result.push_back(v*scalar);
     }
-    return ret;
+    return result;
 }
 
 bool test_scalar_multiplication() {
     std::vector<double> test = {0, 2, 3};
-    std::vector<double> result = scalar_multiplication(test, 2.0);
+    std::vector<double> result = scalar_multiplication(test, -2.0);
 
     
-    if(result[0] == 0 && result[1] == 4 && result[2] == 6) {
+    if(result[0] == 0 && result[1] == -4 && result[2] == -6) {
         std::cout<<"test_scalar_multiplication passed\n";
         return true;
     }
     return false;
 }
 
-void add_vector(std::vector<double>& v1, std::vector<double>& v2) {
-    //adds v2 to v1 in place
+std::vector<double> add_vector(const std::vector<double>& v1, const std::vector<double>& v2) {
+    std::vector<double> result;
     for(int i = 0; i < v1.size(); i++) {
-        v1[i] = v1[i] + v2[i];
+        result.push_back(v1[i] + v2[i]);
     }
+
+    return result;
 }
 
 bool test_add_vector() {
-    
+    std::vector<double> v1 = {1, 2, 3.5};
+    std::vector<double> v2 = {2, 0, 6};
+
+    std::vector<double> result = add_vector(v1, v2);
+
+    if(result[0] == 3 && result[1] == 2 && result[2] == 9.5) {
+        std::cout<<"test_add_vector passed\n";
+        return true;
+    }
+
+    return false;
 }
 
 double calculate_distance(const std::vector<double>& v1, const std::vector<double>& v2) {
@@ -59,11 +73,8 @@ class Particle {
     
     //allow access to full vectors and named individual values
     std::vector<double> position = {0, 0, 0};
-    double& x = position[0]; double& y = position[1]; double& z = position[2];
     std::vector<double> velocity = {0, 0, 0};
-    double& vx = velocity[0]; double& vy = velocity[1]; double& vz = velocity[2];
     std::vector<double> force = {0, 0, 0};
-    double& fx = force[0]; double& fy = force[1]; double& fz = force[2];
 
     Particle(std::vector<double>& params) {
         mass = params[0];
@@ -72,12 +83,12 @@ class Particle {
         force[0] = params[7]; force[1] = params[8]; force[2] = params[9]; 
     }
 
-    std::vector<double> calculate_force(const Particle& p2) {
+    std::vector<double> calculate_force(const Particle& p2) const {
         double distance = calculate_distance(position, p2.position) + SOFTENING_FACTOR;
 
         double gravitational_force = (G * mass * p2.mass) / pow(distance, 2);
 
-        std::vector<double> direction = {position[0] - p2.position[0], position[1] - p2.position[1], position[2] - p2.position[2]};
+        std::vector<double> direction = {p2.position[0] - position[0], p2.position[1] - position[1], p2.position[2] - position[2]};
         std::vector<double> normalized_direction = scalar_multiplication(direction, 1/distance);
         std::vector<double> force_vector = scalar_multiplication(normalized_direction, gravitational_force);
 
@@ -89,28 +100,29 @@ class Particle {
         std::vector<double> velocity_diff = scalar_multiplication(acceleration, delta_t);
 
         //add new velocity
-        add_vector(velocity, velocity_diff);
+        velocity = add_vector(velocity, velocity_diff);
 
         //add new position
         std::vector<double> position_diff = scalar_multiplication(velocity, delta_t);
-        add_vector(position, position_diff);
+        position = add_vector(position, position_diff);
+        //this->print();
     }
 
     void reset_force() {
         this->force = {0, 0, 0};
     }
 
-    void print() {
-        std::cout<<"Mass: "<<mass<<", Position: ["<<x<<", "<<y<<", "<<z<<"]"<<", Velocity: ["<<vx<<", "<<vy<<", "<<vz<<"]\n";
+    void print() const {
+        std::cout<<"Mass: "<<mass<<", Position: ["<<position[0]<<", "<<position[1]<<", "<<position[2]<<"]"<<", Velocity: ["<<velocity[0]<<", "<<velocity[1]<<", "<<velocity[2]<<"]\n";
     }
 
-    std::string write_particle() const{
-        std::string ret;
-        double params[] = {mass, x, y, z, vx, vy, vz, fx, fy, fz};
+    std::string write_particle() const {
+        std::stringstream ret;
+        double params[] = {mass, position[0], position[1], position[2], velocity[0], velocity[1], velocity[2], force[0], force[1], force[2]};
         for(int i = 0; i < (sizeof(params) / sizeof(params[0])); i++) {
-            ret += std::to_string(params[i]) + "\t";
+            ret << params[i] << "\t";
         }
-        return ret;
+        return ret.str();
     }
 };
 
@@ -127,11 +139,11 @@ class State {
             for(int j = i+1; j < particles.size(); j++) {
                 //force applied to particle i by particle j
                 std::vector<double> force_vector = particles[i].calculate_force(particles[j]);
-                add_vector(particles[i].force, force_vector);
+                particles[i].force = add_vector(particles[i].force, force_vector);
 
                 //force applied to particle j will be the same, but opposite direction
                 std::vector<double> inv_force_vector = scalar_multiplication(force_vector, -1);
-                add_vector(particles[j].force, inv_force_vector);
+                particles[j].force = add_vector(particles[j].force, inv_force_vector);
             }
         }
     }
@@ -148,13 +160,13 @@ class State {
         }
     }
 
-    void print_particles() {
-        for(Particle &p: particles) {
-            p.print();
-        }
-    }
+    // void print_particles() {
+    //     for(const Particle &p: particles) {
+    //         p.print();
+    //     }
+    // }
 
-    std::string write_state() const{
+    std::string write_state() const {
         std::string state_string = std::to_string(particles.size()) + "\t";
 
         for(const Particle &p: particles) {
@@ -181,12 +193,16 @@ class State {
 };
 
 
-double random_double(int min, int max) {
-    return (std::rand() % (max-min)) + min;
+double random_double(double min, double max) {
+    std::random_device rd; //pseudorandom number, generates seed for next step
+    std::mt19937 gen(rd()); //better pseudorandom number
+    std::uniform_real_distribution<> distribution(min, max); //maps randomness to a number within the uniform distribution
+
+    return distribution(gen);
 }
 
 State random_initialization(int n_particles) {
-    int max = 1000000;
+    double max = 1000000000.0;
 
     State state;
 
@@ -205,16 +221,23 @@ State random_initialization(int n_particles) {
 State file_initialization(std::string filepath) {
     std::ifstream f(filepath);
 
+    //ensure filestream is open
+    if(!f.is_open()) {
+        std::cout<<"File did not open successfully, check your input filepath\n";
+        exit(0);
+    }
+
     State state;
 
     int n_particles;
     f >> n_particles;
 
     for(int i = 0; i < n_particles; i++) {
+        const int ENTRIES_PER_PARTICLE = 10;
 
         //make particle parameter vector
         std::vector<double> params;
-        for(int i = 0; i < 10; i++){
+        for(int j = 0; j < ENTRIES_PER_PARTICLE; j++){
             double d;
             f >> d;
             params.push_back(d);
@@ -241,18 +264,50 @@ void run_simulation(State &s, std::string output_filepath, double delta_t, int n
     }
 }
 
+bool is_integer(const std::string& s) {
+    for(char c: s) {
+        if(!std::isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]) {
-    std::cout<<test_scalar_multiplication();
+    if(argc != 6){
+        std::cout<<"Use the following arguments to run on command line:\n";
+        std::cout<<"Arg 1: either an integer representing the number of particles (for a random initialization), or a path to an initial state (such as solar.tsv)\n";
+        std::cout<<"Arg 2: (string) filepath to an output tsv file. The program will create it if it doesn't exist, or overwrite if it does\n";
+        std::cout<<"Arg 3: (double) delta T for each step of the simulation\n";
+        std::cout<<"Arg 4: (int) number of timesteps for the simulation\n";
+        std::cout<<"Arg 5: (int) how frequently to write the state. The simulation will write every n timesteps\n";
+        return 0;
+    }
+    
+    //handle command line arguments
+    State s;
+    if(is_integer(argv[1])) {
+        s = random_initialization(std::stoi(argv[1]));
+    } else {
+        s = file_initialization(argv[1]);
+    }
+    std::string output_file = argv[2];
+    double delta_t = std::stod(argv[3]);
+    int n_timesteps = std::stoi(argv[4]);
+    int dump_every_n = std::stoi(argv[5]);
 
-    return 0;
+    //record time of execution
+    namespace chrn = std::chrono;
+    auto start = chrn::high_resolution_clock::now();
 
+    run_simulation(s, output_file, delta_t, n_timesteps, dump_every_n);
 
+    auto end = chrn::high_resolution_clock::now();
+    auto elapsed_us = chrn::duration_cast<chrn::microseconds>(end - start).count();
+    double elapsed_ms = elapsed_us / 1000.0;
 
-    //State s = random_initialization(5);
-    State s = file_initialization("solar.tsv");
-    //s.dump_state("output.tsv");
-    run_simulation(s, "output.tsv", 10.0, 100, 1);
+    std::cout<<"Execution time: "<<elapsed_ms<<"ms\n";
+
     return 0;
 }
 
